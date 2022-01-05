1de1a203b6eddd9e29ab9d8fe14d6ede565e44c7
Mariabraao <marianabarbosaabraao@gmail.com>
2022-01-04 23:15:54 -0300

    Initial Commit

---
+++ Prism Calculator/Prism_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prism_CalculatorApp.swift
+//  Prism Calculator
+//
+//  Created by Mariana Abraão on 04/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Prism_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
